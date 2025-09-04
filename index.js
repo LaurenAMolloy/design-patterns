97e82d31e4aa9a0a2f93c73cca0bdccf102dd6a4
lauren_m <laurenamolloy@gmail.com>
2025-09-04 10:22:35 +0300

    Added movie data tempalte for stats

--- index.js
+++ index.js
@@ -101,8 +101,6 @@ const onInput = async e => {
             onMovieSelect(movie)
         });
 
-
-
         resultsWrapper.appendChild(option);
     }
 };
@@ -130,6 +128,46 @@ const onMovieSelect = async movie => {
            i: "tt0848228"
         }
     });
+    //console.log(response.data);
+    document.querySelector('#summary').innerHTML = movieTemplate(response.data)
+};
 
-    console.log(response.data);
-}
+const movieTemplate = (movieDetail) => {
+    return `
+    <article class="media">
+    <figure class="media-left">
+        <p class=image>
+        <img src="${movieDetail.Poster}" />
+        </p>
+    </figure>
+    <div class="media-content></div>
+    <div class="content>
+    <h1>${movieDetail.Title}</h1>
+    <h4>${movieDetail.Genre}</h4>
+    <p>${movieDetail.Plot}</p>
+    </div>
+    </article>
+    <article class="notification is-primary">
+    <p class="title">${movieDetail.Awards}</p>
+    <p class="subtitle">Awards</p>
+    </article>
+    <article class="notification is-primary">
+    <p class="title">${movieDetail.BoxOffice}</p>
+    <p class="subtitle">Box Office</p>
+    </article>
+    <article class="notification is-primary">
+    <p class="title">${movieDetail.Metascore}</p>
+    <p class="subtitle">Awards</p>
+    </article>
+    <article class="notification is-primary">
+    <p class="title">${movieDetail.imdbRating}</p>
+    <p class="subtitle">IMDB Rating</p>
+    </article>
+    <article class="notification is-primary">
+    <p class="title">${movieDetail.imdbVotes}</p>
+    <p class="subtitle">IMDB Votes</p>
+    </article>
+    
+
+    `
+};
